a819b467985ba7c9fbba885b01aad9719f0f4a4b
duskobogdanovski <dule.bogdanovski@gmail.com>
2021-03-11 11:21:36 +0100

    Rename fanstatic to assets

--- ckanext/resourcedictionary/plugin.py
+++ ckanext/resourcedictionary/plugin.py
@@ -17,7 +17,7 @@ class ResourcedictionaryPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
+        toolkit.add_resource('assets',
                              'resourcedictionary')
 
     # IBlueprint
